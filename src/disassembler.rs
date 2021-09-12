46a85749244de89988e9663bfed99488e3dd0e85
Mikael Mello <git@mikaelmello.com>
2021-09-11 22:13:56 -0300

    Disassembler prints to stderr

--- src/disassembler.rs
+++ src/disassembler.rs
@@ -18,38 +18,38 @@ impl<'a> Disassembler<'a> {
     }
 
     pub fn instruction(&self, idx: usize, instruction: &'a Instruction) {
-        print!("{:04} ", idx);
+        eprint!("{:04} ", idx);
 
         let line = self.chunk.get_line(idx);
 
         if idx > 0 && line == self.chunk.get_line(idx - 1) {
-            print!("   | ");
+            eprint!("   | ");
         } else {
-            print!("{:4} ", line);
+            eprint!("{:4} ", line);
         }
 
         match instruction {
-            Instruction::RETURN => println!("RETURN"),
-            Instruction::ADD => println!("ADD"),
-            Instruction::SUBTRACT => println!("SUBTRACT"),
-            Instruction::MULTIPLY => println!("MULTIPLY"),
-            Instruction::DIVIDE => println!("DIVIDE"),
-            Instruction::NEGATE => println!("NEGATE"),
-            Instruction::NOT => println!("NOT"),
+            Instruction::RETURN => eprintln!("RETURN"),
+            Instruction::ADD => eprintln!("ADD"),
+            Instruction::SUBTRACT => eprintln!("SUBTRACT"),
+            Instruction::MULTIPLY => eprintln!("MULTIPLY"),
+            Instruction::DIVIDE => eprintln!("DIVIDE"),
+            Instruction::NEGATE => eprintln!("NEGATE"),
+            Instruction::NOT => eprintln!("NOT"),
             Instruction::CONSTANT(addr) => self.constant("CONSTANT", *addr),
-            Instruction::TRUE => println!("TRUE"),
-            Instruction::FALSE => println!("FALSE"),
-            Instruction::EQUAL => println!("EQUAL"),
-            Instruction::GREATER => println!("GREATER"),
-            Instruction::LESS => println!("LESS"),
-            Instruction::PRINT(nl) => println!("PRINT nl:{}", nl),
-            Instruction::POP => println!("POP"),
-            Instruction::HALT => println!("HALT"),
+            Instruction::TRUE => eprintln!("TRUE"),
+            Instruction::FALSE => eprintln!("FALSE"),
+            Instruction::EQUAL => eprintln!("EQUAL"),
+            Instruction::GREATER => eprintln!("GREATER"),
+            Instruction::LESS => eprintln!("LESS"),
+            Instruction::PRINT(nl) => eprintln!("PRINT nl:{}", nl),
+            Instruction::POP => eprintln!("POP"),
+            Instruction::HALT => eprintln!("HALT"),
         }
     }
 
     fn constant(&self, name: &str, addr: u16) {
         let value = self.chunk.get_constant(addr);
-        println!("{:16} {:4} '{}'", name, addr, value);
+        eprintln!("{:16} {:4} '{}'", name, addr, value);
     }
 }
